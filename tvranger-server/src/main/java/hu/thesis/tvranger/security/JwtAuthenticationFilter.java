83281b72f33649cd43500204e3fa92b380fa8f3b
Marci <marcelldevelopment@gmail.com>
2018-10-25 14:25:00 +0200

    Finished the implementation of the JwtauthenticationFilter

--- tvranger-server/src/main/java/hu/thesis/tvranger/security/JwtAuthenticationFilter.java
+++ tvranger-server/src/main/java/hu/thesis/tvranger/security/JwtAuthenticationFilter.java
@@ -2,7 +2,13 @@ package hu.thesis.tvranger.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -13,9 +19,43 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
   private static final Logger log = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
-  @Override protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-  
+  @Autowired
+  JwtTokenProvider jwtTokenProvider;
+
+  @Autowired
+  CustomUserDetailsService customUserDetailsService;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+    try {
+      String jsonWebtoken = getJsonWebtokenFromRequest(httpServletRequest);
+
+      if(StringUtils.hasText(jsonWebtoken) && jwtTokenProvider.validateToken(jsonWebtoken)){
+        Long userId = jwtTokenProvider.getUserIdFromJwt(jsonWebtoken);
+
+        UserDetails userDetails = customUserDetailsService.loadUserById(userId);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+      }
+
+    } catch (Exception e){
+      log.error("Could not set user in security context", e);
+    }
+    filterChain.doFilter(httpServletRequest,httpServletResponse);
+  }
+
+  private String getJsonWebtokenFromRequest(HttpServletRequest request){
+    String bearerToken = request.getHeader("Authorization");
+
+    if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+      return bearerToken.substring(7, bearerToken.length());
+    }
+    return null;
   }
 }
